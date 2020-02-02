c75f4832136707cd43278ead468c5feb427f30ac
Richard Cho <96richardohc@gmail.com>
2020-02-02 23:20:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,8 @@
 def prime?(num) 
   range = Array (2..num)
-  if range.detect{|i| i !== num && num 
+  if range.detect{|i| i !== num && num % i == 0} !== nil
+    false
+  else
+    true
+  end
 end
