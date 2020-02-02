9415290e8c886e85540f6c1cf746386dcf3ba2ae
Richard Cho <96richardohc@gmail.com>
2020-02-02 23:25:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,11 @@ def prime?(num)
   puts num
   range = Array (2..num-1)
   range.each do |i|
-    if 
+    if num % i == 0
+      false
+    end
   end
+  true
   range
   # if range.detect{|i| num % i == 0} 
   #   false
